f4acc8923c518fc2e954f5eec65cd0d86e7fed7b
batro83 <batro_ska@yahoo.es>
2020-02-21 09:38:43 +0100

    init

---
+++ src/main/java/com/app/willywonkafactory/WillyWonkaFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.app.willywonkafactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WillyWonkaFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WillyWonkaFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/willywonkafactory/WillyWonkaFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.willywonkafactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WillyWonkaFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
